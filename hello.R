ba31f2ee521c7a470a73ab387ec4d288e48dadea
Anna Khazenzon <annakhaz@stanford.edu>
2016-06-27 11:33:34 -0700

    integrate render

--- hello.R
+++ hello.R
@@ -25,3 +25,5 @@ summary(cars)
 plot(pressure)
 #'
 #' Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
+
+rmarkdown::render("hello.R")
